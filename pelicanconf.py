1693a1441992aa6d3b95f7716498178f0748e646
Fred (feedoo) Blain <f.blain@sheffield.ac.uk>
2017-02-28 00:19:44 +0000

    added: theme files, publi & pelican configuration files, fabfile

---
+++ pelicanconf.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Fred Blain'
+SITENAME = 'Fred Blain'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/London'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = ()
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+PLUGINS = ['pelican_publications']
+
+# theming
+
+HIDE_SIDEBAR = True
+DISPLAY_PAGES_ON_MENU = False
+DISPLAY_CATEGORIES_ON_MENU = False
+THEME = 'theme'
+
